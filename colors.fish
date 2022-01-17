25721fdea58058af4739a48dbb1fea9e1654d5bc
jin <jeromesuh13@gmail.com>
2022-01-16 21:40:19 -0500

    fish color export/import

---
+++ colors.fish
@@ -0,0 +1,30 @@
+#!/usr/bin/env fish
+
+set -U fish_color_autosuggestion 4c566a
+set -U fish_color_cancel -r
+set -U fish_color_command d08770
+set -U fish_color_comment 434c5e
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end 88c0d0
+set -U fish_color_error ebcb8b
+set -U fish_color_escape 00a6b2
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_host_remote yellow
+set -U fish_color_match --background=brblue
+set -U fish_color_normal normal
+set -U fish_color_operator 00a6b2
+set -U fish_color_param 81a1c1
+set -U fish_color_quote a3be8c
+set -U fish_color_redirection b48ead
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+echo "[31mF[31mi[31ms[31mh[31m [31mc[38;5;208mo[38;5;208ml[38;5;208mo[38;5;208mr[33ms[33m [33mh[33ma[32mv[32me[32m [32mb[34me[34me[34mn[34m [35ms[35me[35mt[m"
