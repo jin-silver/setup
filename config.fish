00eaab8b129867f1f910419b82b234c1871ea611
jin-silver <jeromesuh13@gmail.com>
2022-02-05 18:08:01 -0500

    changed key bindings

--- config.fish
+++ config.fish
@@ -4,9 +4,18 @@ and not set -q TMUX
 end
 
 function fish_greeting
-    fortune
+    cat ~/.config/fish/moon
 end
 
+function fish_user_key_bindings
+    fish_vi_key_bindings
+    bind -M insert -m default jj backward-char force-repaint
+    bind -M default \cr history-search-backward
+    bind -M insert \cr history-search-backward
+    bind -M default \cf history-search-forward
+    bind -M insert \cf history-search-forward
+    bind -M insert \ce end-of-line
+end
 alias ls='/opt/coreutils/bin/ls --color --group-directories-first -X'
 alias ghidra='/home/jin/ghidra_10.1-BETA_PUBLIC/ghidraRun'
 alias pattern_create='/home/jin/gitforks/metasploit-framework/tools/exploit/pattern_create.rb'
@@ -16,4 +25,3 @@ alias msfvenom='/home/jin/gitforks/metasploit-framework/msfvenom'
 wal -qni ~/Pictures/groot.jpg
 
 export LS_COLORS=(vivid generate ~/.config/vivid/iceberg-dark.yml)
-fish_vi_key_bindings
