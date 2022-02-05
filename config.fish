99512cbbd29137eae5875d1cf52abd6ccd6d4941
jin-silver <jeromesuh13@gmail.com>
2022-02-05 16:03:44 -0500

    added vi mode on command line

--- config.fish
+++ config.fish
@@ -8,8 +8,12 @@ function fish_greeting
 end
 
 alias ls='/opt/coreutils/bin/ls --color --group-directories-first -X'
-
+alias ghidra='/home/jin/ghidra_10.1-BETA_PUBLIC/ghidraRun'
+alias pattern_create='/home/jin/gitforks/metasploit-framework/tools/exploit/pattern_create.rb'
+alias pattern_offset='/home/jin/gitforks/metasploit-framework/tools/exploit/pattern_offset.rb'
+alias msfvenom='/home/jin/gitforks/metasploit-framework/msfvenom'
 
 wal -qni ~/Pictures/groot.jpg
 
 export LS_COLORS=(vivid generate ~/.config/vivid/iceberg-dark.yml)
+fish_vi_key_bindings
